9bf26602bd6c41f90ffa69ff12698ea3ee45768b
Luiz Cesar Pacheco <lcpsantos@gmail.com>
2022-09-11 19:43:38 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { BASE_URL } from '../../utils/request';
 
 import './styles.css';
+import { toast } from 'react-toastify';
 
 type Props = {
   saleId: number;
@@ -12,7 +13,7 @@ type Props = {
 function handleClick(id : number) {
   axios(`${BASE_URL}/sale/${id}/notification`)
   .then(response => {
-    console.log("Sucesso");
+    toast.info("SMS enviado com sucesso!");
   })
 }
 
